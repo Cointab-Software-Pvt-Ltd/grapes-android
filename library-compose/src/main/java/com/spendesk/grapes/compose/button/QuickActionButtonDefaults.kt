2cde15dd9e69d0c14a8aaee42a6e32c1e955d8e8
Kelian CLERC <kelian.clerc@spendesk.com>
2024-10-30 14:49:17 +0100

    Create QuickAction defaults

---
+++ library-compose/src/main/java/com/spendesk/grapes/compose/button/QuickActionButtonDefaults.kt
@@ -0,0 +1,46 @@
+package com.spendesk.grapes.compose.button
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.ButtonColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+import com.spendesk.grapes.compose.theme.GrapesTheme
+
+/**
+ * @author : kelian
+ * @since : 30/10/2024
+ **/
+@Immutable
+object QuickActionButtonDefaults {
+
+    val buttonSize = 48.dp
+
+    val buttonColors: ButtonColors
+        @Composable
+        @ReadOnlyComposable
+        get() = ButtonColors(
+            containerColor = GrapesTheme.colors.structureSurface,
+            contentColor = GrapesTheme.colors.neutralDarker,
+            disabledContainerColor = GrapesTheme.colors.neutralLight,
+            disabledContentColor = GrapesTheme.colors.structureSurface
+        )
+
+    val buttonBorder: BorderStroke
+        @Composable
+        @ReadOnlyComposable
+        get() = BorderStroke(1.dp, GrapesTheme.colors.neutralLighter)
+
+    val buttonShape: Shape
+        @Composable
+        @ReadOnlyComposable
+        get() = GrapesTheme.shapes.shape2
+
+    val buttonPadding: PaddingValues
+        @Composable
+        @ReadOnlyComposable
+        get() = PaddingValues(GrapesTheme.dimensions.spacing3)
+}
