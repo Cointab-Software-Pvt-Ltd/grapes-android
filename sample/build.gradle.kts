948a0500f454cb8da4b9f19756cc6f364b305f1f
Kelian CLERC <kelian.clerc@spendesk.com>
2025-01-17 14:54:32 +0100

    Setup navigation dependencies

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.hilt)
     alias(libs.plugins.firebase.appdistribution)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -104,4 +105,8 @@ dependencies {
 
     // REFLECTION
     implementation(libs.kotlinx.reflect)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization)
 }
