15af3e870c312e810e3eb09849921c5b1098b243
Kelian CLERC <kelian.clerc@spendesk.com>
2025-01-21 16:20:59 +0100

    Create tooltip composable

---
+++ sample/src/main/java/com/spendesk/grapes/samples/compose/helper/Tooltip.kt
@@ -0,0 +1,52 @@
+package com.spendesk.grapes.samples.compose.helper
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import com.spendesk.grapes.compose.icons.GrapesIconSet
+import com.spendesk.grapes.compose.theme.GrapesTheme
+
+/**
+ * @author Kélian CLERC
+ * @since 21/01/2025
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GrapesTooltipBox(text: String, modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+        state = rememberTooltipState(isPersistent = false),
+        tooltip = { TooltipContent(text, modifier) }
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun TooltipContent(text: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .clip(GrapesTheme.shapes.radius4)
+            .background(color = GrapesTheme.colors.backgroundSecondaryInfoDefault)
+            .padding(GrapesTheme.dimensions.spacing2),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(GrapesTheme.dimensions.spacing1),
+    ) {
+        GrapesIconSet.CircleInformation(tint = GrapesTheme.colors.contentInfoDefault)
+        Text(
+            text = text,
+            style = GrapesTheme.typography.bodyM,
+            color = GrapesTheme.colors.contentInfoDefault
+        )
+    }
+}
