c22569f84cd28d62cc93c62f54040a043cc80345
Kelian CLERC <kelian.clerc@spendesk.com>
2025-01-22 10:42:11 +0100

    Update typography

--- library-compose/src/main/java/com/spendesk/grapes/compose/theme/Typography.kt
+++ library-compose/src/main/java/com/spendesk/grapes/compose/theme/Typography.kt
@@ -2,9 +2,11 @@ package com.spendesk.grapes.compose.theme
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.spendesk.grapes.compose.R
@@ -14,33 +16,63 @@ import com.spendesk.grapes.compose.R
  * @since : 03/05/2022, Tue
  **/
 
-private val DefaultGrapesFontFamily = FontFamily(
-    Font(R.font.font_raw_gt_america_regular, FontWeight.Normal),
-    Font(R.font.font_raw_gt_america_medium, FontWeight.Medium)
+private val Roobert = FontFamily(
+    Font(R.font.font_roobert_medium, FontWeight.Medium),
+)
+
+@OptIn(ExperimentalTextApi::class)
+private val Inter = FontFamily(
+    Font(
+        R.font.font_inter_opsz_wght,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(600),
+            FontVariation.width(100f),
+            FontVariation.slant(0f),
+        ),
+        weight = FontWeight.SemiBold
+    ),
+    Font(
+        R.font.font_inter_opsz_wght,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(500),
+            FontVariation.width(100f),
+            FontVariation.slant(0f),
+        ),
+        weight = FontWeight.Medium
+    )
 )
 
 @Immutable
 data class GrapesTypography(
+    val bodyXs: TextStyle,
     val bodyS: TextStyle,
     val bodyM: TextStyle,
+    @Deprecated("No longer used")
     val bodyL: TextStyle,
+    @Deprecated("No longer used")
     val bodyXl: TextStyle,
+    @Deprecated("No longer used")
     val bodyXxl: TextStyle,
     val titleS: TextStyle,
     val titleM: TextStyle,
     val titleL: TextStyle,
     val titleXl: TextStyle,
-    val titleXxl: TextStyle
+    val titleXxl: TextStyle,
+    val heading: TextStyle,
 ) {
     constructor(
-        defaultFontFamily: FontFamily = DefaultGrapesFontFamily,
-        bodyS: TextStyle = TextStyle(
-            fontWeight = FontWeight.Normal,
+        defaultFontFamily: FontFamily = Inter,
+        bodyXs: TextStyle = TextStyle(
+            fontWeight = FontWeight.Medium,
             fontSize = 12.sp,
         ),
-        bodyM: TextStyle = TextStyle(
-            fontWeight = FontWeight.Normal,
+        bodyS: TextStyle = TextStyle(
+            fontWeight = FontWeight.Medium,
             fontSize = 14.sp,
+        ),
+        bodyM: TextStyle = TextStyle(
+            fontWeight = FontWeight.Medium,
+            fontSize = 16.sp,
         ),
         bodyL: TextStyle = TextStyle(
             fontWeight = FontWeight.Normal,
@@ -56,25 +88,31 @@ data class GrapesTypography(
         ),
         titleS: TextStyle = TextStyle(
             fontWeight = FontWeight.Medium,
-            fontSize = 12.sp,
-        ),
-        titleM: TextStyle = TextStyle(
-            fontWeight = FontWeight.Medium,
             fontSize = 14.sp,
         ),
-        titleL: TextStyle = TextStyle(
-            fontWeight = FontWeight.Medium,
+        titleM: TextStyle = TextStyle(
+            fontWeight = FontWeight.SemiBold,
             fontSize = 16.sp,
         ),
+        titleL: TextStyle = TextStyle(
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 18.sp,
+        ),
         titleXl: TextStyle = TextStyle(
-            fontWeight = FontWeight.Medium,
+            fontWeight = FontWeight.SemiBold,
             fontSize = 20.sp,
         ),
         titleXxl: TextStyle = TextStyle(
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 24.sp,
+        ),
+        heading: TextStyle = TextStyle(
+            fontFamily = Roobert,
             fontWeight = FontWeight.Medium,
             fontSize = 32.sp,
-        )
+        ),
     ) : this(
+        bodyXs = bodyXs.withDefaultFontFamily(defaultFontFamily),
         bodyS = bodyS.withDefaultFontFamily(defaultFontFamily),
         bodyM = bodyM.withDefaultFontFamily(defaultFontFamily),
         bodyL = bodyL.withDefaultFontFamily(defaultFontFamily),
@@ -84,7 +122,8 @@ data class GrapesTypography(
         titleM = titleM.withDefaultFontFamily(defaultFontFamily),
         titleL = titleL.withDefaultFontFamily(defaultFontFamily),
         titleXl = titleXl.withDefaultFontFamily(defaultFontFamily),
-        titleXxl = titleXxl.withDefaultFontFamily(defaultFontFamily)
+        titleXxl = titleXxl.withDefaultFontFamily(defaultFontFamily),
+        heading = heading,
     )
 }
 
