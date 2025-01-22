6c5f42a94dcc1daf73a6c490f3a50b34617e9211
Kelian CLERC <kelian.clerc@spendesk.com>
2025-01-22 11:24:13 +0100

    Remove view extensions

--- library/src/main/java/com/spendesk/grapes/extensions/ViewExtensions.kt
+++ library/src/main/java/com/spendesk/grapes/extensions/ViewExtensions.kt
@@ -5,7 +5,6 @@ import android.content.res.ColorStateList
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.RippleDrawable
 import android.graphics.drawable.StateListDrawable
-import android.os.Build
 import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
@@ -32,7 +31,7 @@ internal fun View.setRippleDrawable(
     @ColorRes colorDisableId: Int,
     @DimenRes radiusId: Int,
     @DimenRes strokeId: Int = 0,
-    @ColorRes strokeColorId: Int = 0
+    @ColorRes strokeColorId: Int = 0,
 ) {
     val backgroundColor = context.colorCompat(colorId)
     val colorPressed = context.colorCompat(colorPressedId)
@@ -59,7 +58,7 @@ internal fun View.setDrawable(
     @DimenRes radiusId: Int = 0,
     @DimenRes strokeSizeId: Int = 0,
     @ColorRes strokeColorId: Int = 0,
-    shape: Int = GradientDrawable.RECTANGLE
+    shape: Int = GradientDrawable.RECTANGLE,
 ) {
     val backgroundColor = context.colorCompat(colorId)
     val strokeDimen = if (strokeSizeId != 0) resources.getDimensionPixelOffset(strokeSizeId) else 0
@@ -155,11 +154,9 @@ internal fun View.setPaddingTop(paddingTop: Int) {
 }
 
 internal fun View.addRippleEffect() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        val outValue = TypedValue()
-        context.theme.resolveAttribute(android.R.attr.selectableItemBackground, outValue, true)
-        foreground = ContextCompat.getDrawable(context, outValue.resourceId)
-    }
+    val outValue = TypedValue()
+    context.theme.resolveAttribute(android.R.attr.selectableItemBackground, outValue, true)
+    foreground = ContextCompat.getDrawable(context, outValue.resourceId)
     isClickable = true
     isFocusable = true
 }
