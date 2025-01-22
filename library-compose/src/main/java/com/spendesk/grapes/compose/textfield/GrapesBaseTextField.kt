5916da606d2398b0eb4591bdaba498052620ba30
Kelian CLERC <kelian.clerc@spendesk.com>
2025-01-22 14:59:16 +0100

    Migrate bodyS to bodyXs

--- library-compose/src/main/java/com/spendesk/grapes/compose/textfield/GrapesBaseTextField.kt
+++ library-compose/src/main/java/com/spendesk/grapes/compose/textfield/GrapesBaseTextField.kt
@@ -240,7 +240,7 @@ internal fun GrapesHelperText(
     text: String,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    textStyle: TextStyle = GrapesTheme.typography.bodyS,
+    textStyle: TextStyle = GrapesTheme.typography.bodyXs,
     isError: Boolean = false,
     colors: GrapesTextFieldColors = GrapesTextFieldDefaults.textFieldColors(),
     contentPadding: PaddingValues = GrapesTextFieldDefaults.textFieldPadding(),
