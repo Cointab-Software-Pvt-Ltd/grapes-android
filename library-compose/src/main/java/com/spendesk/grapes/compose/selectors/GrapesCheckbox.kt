9aee0714025d806487371862aa48199b7ec2d4fc
Romain Goutte-Fangeas <romain.goutte-fangeas@spendesk.com>
2025-04-30 15:48:33 +0200

    Update checkbox colors

--- library-compose/src/main/java/com/spendesk/grapes/compose/selectors/GrapesCheckbox.kt
+++ library-compose/src/main/java/com/spendesk/grapes/compose/selectors/GrapesCheckbox.kt
@@ -5,11 +5,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.CheckboxColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.spendesk.grapes.compose.theme.GrapesTheme
@@ -24,21 +25,31 @@ fun GrapesCheckbox(
     onCheckedChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
     isEnabled: Boolean = true,
-) =
+) {
     Checkbox(
         modifier = modifier,
         checked = isChecked,
         enabled = isEnabled,
-        colors = CheckboxDefaults.colors(
-            checkedColor = GrapesCheckboxDefaultColors.checkedColor,
-            uncheckedColor = GrapesCheckboxDefaultColors.uncheckedColor,
-            checkmarkColor = GrapesCheckboxDefaultColors.checkmarkColor,
-            disabledCheckedColor = GrapesCheckboxDefaultColors.disabledCheckedColor,
-            disabledUncheckedColor = GrapesCheckboxDefaultColors.disabledUncheckedColor,
-            disabledIndeterminateColor = GrapesCheckboxDefaultColors.disabledIndeterminateColor
+        colors = CheckboxColors(
+            // Checkmark color
+            checkedCheckmarkColor = GrapesTheme.colors.contentComplementary,
+            uncheckedCheckmarkColor = Color.Transparent,
+            // Box color
+            checkedBoxColor = GrapesTheme.colors.backgroundPrimaryBrandDefault,
+            uncheckedBoxColor = Color.Transparent,
+            disabledCheckedBoxColor = GrapesTheme.colors.contentDisable,
+            disabledUncheckedBoxColor = Color.Transparent,
+            disabledIndeterminateBoxColor = GrapesTheme.colors.contentDisable,
+            // Border color
+            checkedBorderColor = GrapesTheme.colors.backgroundPrimaryBrandDefault,
+            uncheckedBorderColor = GrapesTheme.colors.borderDefault,
+            disabledBorderColor = GrapesTheme.colors.contentDisable,
+            disabledUncheckedBorderColor = GrapesTheme.colors.borderDefault,
+            disabledIndeterminateBorderColor = GrapesTheme.colors.borderDefault,
         ),
         onCheckedChange = onCheckedChange
     )
+}
 
 @Composable
 @Preview(
