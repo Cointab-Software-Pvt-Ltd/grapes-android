43f1cb85d5fc94c74cc6b7aca91ebcaadfdfe43a
RomainGF <romain.goutte-fangeas@spendesk.com>
2025-06-25 10:20:38 +0200

    Fix depreciations

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -14,7 +14,6 @@ android {
 
     defaultConfig {
         minSdk = libs.versions.androidMinSdk.get().toInt()
-        targetSdk = libs.versions.androidTargetSdk.get().toInt()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
