922ef2792e0b5e7df43414050968af0bf72ea6b3
Valerii Bashchevanzhy <valerii.bashchevanzhy@sigma.software>
2025-10-21 16:43:41 +0200

    Remove GrapesSearchInputSecondary

--- library-compose/src/main/java/com/spendesk/grapes/compose/textfield/GrapesSearchInput.kt
+++ library-compose/src/main/java/com/spendesk/grapes/compose/textfield/GrapesSearchInput.kt
@@ -3,7 +3,6 @@ package com.spendesk.grapes.compose.textfield
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -93,73 +92,6 @@ fun GrapesSearchInputPrimary(
     )
 }
 
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-fun GrapesSearchInputSecondary(
-    value: String,
-    placeholder: String,
-    clearContentDescription: String,
-    onValueChange: (String) -> Unit,
-    onClear: () -> Unit,
-    modifier: Modifier = Modifier,
-    helperText: String? = null,
-    enabled: Boolean = true,
-    readOnly: Boolean = false,
-    isError: Boolean = false,
-    onClick: (() -> Unit)? = null,
-    onKeyboardSearch: (() -> Unit)? = null,
-    leadingIcon: @Composable (() -> Unit)? = null,
-) {
-    val keyboardController = LocalSoftwareKeyboardController.current
-
-    GrapesBaseTextField(
-        value = value,
-        placeholderValue = placeholder,
-        onValueChange = onValueChange,
-        modifier = modifier.heightIn(min = GrapesTheme.dimensions.unit40),
-        helperText = helperText,
-        enabled = enabled,
-        readOnly = readOnly,
-        singleLine = true,
-        textStyle = GrapesTheme.typography.bodyL,
-        colors = GrapesTextFieldDefaults.textFieldColors(
-            backgroundColor = GrapesTheme.colors.backgroundSecondaryDefault,
-            leadingIconColor = GrapesTheme.colors.contentDecorativeIcon,
-            trailingIconColor = GrapesTheme.colors.contentDecorativeIcon,
-            textColor = GrapesTheme.colors.contentPrimary,
-            placeholderColor = GrapesTheme.colors.contentSecondaryBGPrimary,
-            unfocusedBorderColor = GrapesTheme.colors.borderDefault,
-            focusedBorderColor = GrapesTheme.colors.borderSelected,
-        ),
-        isError = isError,
-        onClick = onClick,
-        keyboardActions = KeyboardActions(
-            onSearch = {
-                onKeyboardSearch?.invoke()
-                keyboardController?.hide()
-            },
-        ),
-        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-        leadingIcon = leadingIcon,
-        trailingIcon = {
-            if (value.isNotEmpty()) {
-                IconButton(onClick = onClear) {
-                    GrapesIconSet.CircleCross(
-                        contentDescription = clearContentDescription,
-                        tint = GrapesTheme.colors.contentDecorativeIcon,
-                    )
-                }
-            }
-        },
-        textPadding = GrapesTextFieldDefaults.textFieldPadding(
-            bottom = searchInputTextPaddingVertical,
-            end = searchInputTextPaddingHorizontal,
-            start = searchInputTextPaddingHorizontal,
-            top = searchInputTextPaddingVertical,
-        ),
-    )
-}
-
 @Preview
 @Composable
 private fun Preview() {
@@ -179,22 +111,6 @@ private fun Preview() {
                 leadingIcon = { GrapesIconSet.CrossSmall() },
             )
             GrapesSearchInputPrimary(
-                value = "Value",
-                placeholder = "Search",
-                clearContentDescription = "",
-                onValueChange = {},
-                onClear = {},
-                leadingIcon = { GrapesIconSet.CrossSmall() },
-            )
-            GrapesSearchInputSecondary(
-                value = "",
-                placeholder = "Search",
-                clearContentDescription = "",
-                onValueChange = {},
-                onClear = {},
-                leadingIcon = { GrapesIconSet.CrossSmall() },
-            )
-            GrapesSearchInputSecondary(
                 value = "Value",
                 placeholder = "Search",
                 clearContentDescription = "",
